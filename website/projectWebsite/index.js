c53b1e147fd953f16b2325266ebfd0dfd8e322b7
Mateusz Kowalski <towermanxxl@gmail.com>
2025-08-06 23:40:20 +0200

    first version of project website

---
+++ website/projectWebsite/index.js
@@ -0,0 +1,36 @@
+function downloadFile(pathToFile, downloadName){
+    let a = document.createElement("a")
+    a.href = pathToFile
+    a.download = downloadName;
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
+}
+document.getElementById("playButton").addEventListener("click",(event)=>{
+    // change to game init type
+    console.log("event")
+    document.getElementById("welcomeScreen").style.display = "none"
+    document.getElementById("gameSelector").style.display = "flex"
+
+})
+
+document.getElementById("returnToHomePage").addEventListener("click",(event)=>{
+    // change to welcome screen
+    console.log("event")
+    document.getElementById("welcomeScreen").style.display = "flex"
+    document.getElementById("gameSelector").style.display = "none"
+
+})
+
+document.getElementById("linuxPlayButton").addEventListener("click",(event)=>{
+    downloadFile("files/minecraftClone-linux", "minecraftGame-linux")
+})
+document.getElementById("windowsPlayButton").addEventListener("click",(event)=>{
+    downloadFile("files/minecraftClone-win", "minecraftGame-win")
+})
+document.getElementById("webPlayButton").addEventListener("click",(event)=>{
+    // redirect
+})
+document.getElementById("macPlayButton").addEventListener("click",(event)=>{
+    // N/ A
+})
